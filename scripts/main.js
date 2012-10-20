3381713e55db407a3bc59b5955549aa648259917
Peter Lyle-Dugas <p.lyled@gmail.com>
2012-10-19 17:17:46 -0700

    wrote + implemented setLocalTime()

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,29 @@
+function setLocalTime(){
+	var currentTime = new Date();
+	var currentHours = currentTime.getHours();
+	var currentMinutes = currentTime.getMinutes();
+	var abbreviation = "";
+
+	// Convert to 12 hour time
+	if(currentHours > 12) {
+		currentHours -= 12;
+		abbreviation = "PM";
+	} else {
+		abbreviation = "AM";
+	}
+
+	// Normalize minute display under 10
+	if (currentMinutes < 10) {
+		currentMinutes = "0" + currentMinutes;
+	};
+
+	// Format and write current time to the <time> element
+	$displayTime = $('time');
+	$displayTime.empty()
+		.attr('datetime', currentTime.getTime())
+		.append(currentHours + ":" + currentMinutes + abbreviation);
+}
+
 function prepareStations(){
 	$.get('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V', function(data){
 		$departureStation = $('#departureStation');
@@ -10,4 +36,8 @@ function prepareStations(){
 	});
 }
 
+setLocalTime();
+setInterval(function(){
+	setLocalTime();
+}, 1000);
 prepareStations();
